32b4cb59bdb7d252630742094e2cfaf1dc1d6db7
Samuel Siegart <samsiegart@gmail.com>
2022-05-03 19:21:21 -0700

    chore: use wallet-connection component (#19)

--- ui/src/context/Application.jsx
+++ ui/src/context/Application.jsx
@@ -1,27 +1,19 @@
-import React, { createContext, useContext, useReducer, useEffect } from 'react';
+import React, { createContext, useContext, useReducer } from 'react';
 import 'json5';
 import 'utils/installSESLockdown';
 
-import { makeCapTP, E } from '@endo/captp';
-import { Far } from '@endo/marshal';
 import { makeAsyncIterableFromNotifier as iterateNotifier } from '@agoric/notifier';
+import { E } from '@endo/eventual-send';
 
-import {
-  activateWebSocket,
-  deactivateWebSocket,
-  getActiveSocket,
-} from '../utils/fetchWebSocket';
-
+import WalletConnection from '../components/components/WalletConnection';
 import { dappConfig, refreshConfigFromWallet } from '../utils/config';
 
 import {
   reducer,
   defaultState,
   setAssets,
-  resetState,
   setAutoswap,
   setApproved,
-  setError,
   updateOffers,
 } from '../store/store';
 
@@ -78,129 +70,68 @@ export default function Provider({ children }) {
   const [state, dispatch] = useReducer(reducer, defaultState);
   const { brandToInfo } = state;
 
-  useEffect(() => {
-    // Receive callbacks from the wallet connection.
-    const otherSide = Far('needDappApproval', {
-      needDappApproval(_dappOrigin, _suggestedDappPetname) {
-        dispatch(setApproved(false));
-      },
-      dappApproved(_dappOrigin) {
-        dispatch(setApproved(true));
-      },
-    });
+  const retrySetup = async () => {
+    await refreshConfigFromWallet(walletP);
+    const {
+      RUN_ISSUER_BOARD_ID,
+      AMM_INSTALLATION_BOARD_ID,
+      AMM_INSTANCE_BOARD_ID,
+      AMM_NAME,
+    } = dappConfig;
+    console.log('dappConfig', dappConfig);
 
-    let walletAbort;
-    let walletDispatch;
-    activateWebSocket({
-      async onConnect() {
-        const { CONTRACT_NAME, AMM_NAME } = dappConfig;
-
-        const socket = getActiveSocket();
-        const {
-          abort: ctpAbort,
-          dispatch: ctpDispatch,
-          getBootstrap,
-        } = makeCapTP(
-          CONTRACT_NAME,
-          obj => socket.send(JSON.stringify(obj)),
-          otherSide,
-        );
-        walletAbort = ctpAbort;
-        walletDispatch = ctpDispatch;
-        walletP = getBootstrap();
-
-        await refreshConfigFromWallet(walletP);
-        const {
-          RUN_ISSUER_BOARD_ID,
-          AMM_INSTALLATION_BOARD_ID,
-          AMM_INSTANCE_BOARD_ID,
-        } = dappConfig;
-
-        const zoe = E(walletP).getZoe();
-        const board = E(walletP).getBoard();
-
-        if (board) {
-          setApproved(true);
-        }
-        // else{
-        //   setApproved(false);
-        // }
-        await Promise.all([
-          // setupTreasury(dispatch, brandToInfo, zoe, board, INSTANCE_BOARD_ID),
-          setupAMM(dispatch, brandToInfo, zoe, board, AMM_INSTANCE_BOARD_ID),
-        ]);
-
-        // The moral equivalent of walletGetPurses()
-        async function watchPurses() {
-          const pn = E(walletP).getPursesNotifier();
-          for await (const purses of iterateNotifier(pn)) {
-            dispatch(setAssets(purses));
-          }
-        }
-        watchPurses().catch(err =>
-          console.error('FIGME: got watchPurses err', err),
-        );
-
-        async function watchBrands() {
-          console.log('BRANDS REQUESTED');
-          const issuersN = E(walletP).getIssuersNotifier();
-          for await (const issuers of iterateNotifier(issuersN)) {
-            updateBrandPetnames({
-              dispatch,
-              brandToInfo,
-              issuersFromNotifier: issuers,
-            });
-          }
-        }
-        watchBrands().catch(err => {
-          console.error('got watchBrands err', err);
+    const zoe = E(walletP).getZoe();
+    const board = E(walletP).getBoard();
+    if (board) {
+      setApproved(true);
+    }
+    await Promise.all([
+      // setupTreasury(dispatch, brandToInfo, zoe, board, INSTANCE_BOARD_ID),
+      setupAMM(dispatch, brandToInfo, zoe, board, AMM_INSTANCE_BOARD_ID),
+    ]);
+    async function watchPurses() {
+      const pn = E(walletP).getPursesNotifier();
+      for await (const purses of iterateNotifier(pn)) {
+        dispatch(setAssets(purses));
+      }
+    }
+    watchPurses().catch(err => console.error('got watchPurses err', err));
+    async function watchBrands() {
+      console.log('BRANDS REQUESTED');
+      const issuersN = E(walletP).getIssuersNotifier();
+      for await (const issuers of iterateNotifier(issuersN)) {
+        updateBrandPetnames({
+          dispatch,
+          brandToInfo,
+          issuersFromNotifier: issuers,
         });
-        await Promise.all([
-          E(walletP).suggestInstallation(
-            `${AMM_NAME}Installation`,
-            AMM_INSTALLATION_BOARD_ID,
-          ),
-          E(walletP).suggestInstance(
-            `${AMM_NAME}Instance`,
-            AMM_INSTANCE_BOARD_ID,
-          ),
-          E(walletP).suggestIssuer('RUN', RUN_ISSUER_BOARD_ID),
-        ]);
-
-        watchOffers(dispatch);
-      },
-      onDisconnect() {
-        dispatch(setApproved(false));
-        console.log('Running on Disconnect');
-        walletAbort && walletAbort();
-        dispatch(resetState());
-      },
-      onMessage(data) {
-        const obj = JSON.parse(data);
-        console.log('Printing Object empty: ', obj);
-        console.log(!obj.exception);
-        if (obj.exception) {
-          console.log(obj.exception.body);
-          dispatch(
-            setError({
-              name: 'Something went wrong.',
-            }),
-          );
-        } else {
-          console.log('wallet Disconnect:', obj?.payload?.payload === false);
-          if (obj?.payload?.payload === false) {
-            setApproved(false);
-          }
-          walletDispatch && walletDispatch(obj);
-        }
-      },
+      }
+    }
+    watchBrands().catch(err => {
+      console.error('got watchBrands err', err);
     });
-    return deactivateWebSocket;
-  }, []);
+    await Promise.all([
+      E(walletP).suggestInstallation(
+        `${AMM_NAME}Installation`,
+        AMM_INSTALLATION_BOARD_ID,
+      ),
+      E(walletP).suggestInstance(`${AMM_NAME}Instance`, AMM_INSTANCE_BOARD_ID),
+      E(walletP).suggestIssuer('RUN', RUN_ISSUER_BOARD_ID),
+    ]);
+
+    watchOffers(dispatch);
+  };
+
+  const setWalletP = async bridge => {
+    walletP = bridge;
+
+    await retrySetup();
+  };
 
   return (
     <ApplicationContext.Provider value={{ state, dispatch, walletP }}>
       {children}
+      <WalletConnection setWalletP={setWalletP} dispatch={dispatch} />
     </ApplicationContext.Provider>
   );
 }
--- ui/src/store/store.js
+++ ui/src/store/store.js
@@ -13,6 +13,7 @@ export const {
   initial: defaultState,
   actions: {
     setApproved,
+    setConnected,
     mergeBrandToInfo,
     addToBrandToInfo,
     updateOffers,
